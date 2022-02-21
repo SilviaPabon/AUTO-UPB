881ef7dbc26d80f71ed4afdee644d2d855a7479e
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-02-21 12:16:15 -0500

    Signup view
    Se agregaron las rutas y vistas del signup:
    
    Cambios en:
    
    - controller_auth.js: Controlador del signup y login.
    - connection.js: Conexión con la base de datos (aún no se usa).
    - query_db_pi.sql: Se comentaron líneas innecesarias.
    - index.js: Se importaron las rutas del signup y login.
    - _components.scss: Se agregaron los componentes: Title, input-form, label, form-group e input.
    - _signup.scss: Estilos propios de la página de signup.
    - login.ejs: Vista del login con ejs.  (Vacía).
    - signup.ejs: Vista del signup con ejs.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 // -- Settings --
 app.set('port', process.env.PORT || 3000); 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); 
+app.set('views', path.join(__dirname, 'views'));
 
 // -- Static files route --
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,7 +23,7 @@ app.use(express.json());
 
 // -- Routes --
 const router = require('./routes/router.js'); 
-app.use('/users', router.users); // Just an example
+app.use('/', router.authentication); 
 
 // -- Starting the server --
 app.listen(app.get('port'), ()=> {
