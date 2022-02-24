cc2c9687657932f2ac205624f89116c4540fffb9
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-02-23 23:33:29 -0500

    Vista para ingresar los datos de un nuevo accesorio
    Cambios en:
    
    -admin_controller.js: Controlador de la nueva ruta.
    - _componentes.scss: Estilos para el input de tipo descripción.
    - admin.js: Nueva ruta.
    - inventory_select_accessory_type.ejs: Redirección a la nueva página desde el botón.
    - new_accessory.ejs: Nueva vista HTML/EJS para agregar un nuevo producto al inventario.

--- src/controllers/admin_controller.js
+++ src/controllers/admin_controller.js
@@ -5,5 +5,8 @@ controller.inventory = (req, res) => {
     res.render('admin/inventory_select_accessory_type');   
 }; 
 
+controller.inventory_add_new = (req, res) => {
+    res.render('admin/new_accessory');   
+}; 
 
 module.exports = controller; 
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/admin_controller');  
 
 router.get('/inventory', controller.inventory); 
+router.get('/inventory/add_new', controller.inventory_add_new); 
 
 module.exports = router; 
