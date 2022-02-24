dd14dc3ed7c05842692344b4b8ed8f9059c19568
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-02-24 07:07:40 -0500

    Agregar inventario a accesorios existentes.
    Se agregó la vista que muestra al administrador los accesorios existentes (Solo front) y la vista del formulario para agregar inventario a esos accesorios.
    
    Cambios en:
    - admin_controller.js: Controladores de las dos nuevas rutas.
    - _component.scss: Se agregó el componente para botones de tarjetas y títulos de tarjetas.
    - _inventory_select_existing_accessory.scss: Estilos de la vista para mostrar los accesorios existente.
    - admin.js: Rutas nuevas.
    - existing_accessory.ejs: Vista HTML / ejs para agregar inventario a accesorios existentes.
    - inventory_select_acessory_type.ejs: Se agregó el enlace a la nueva ruta para accesorios existentes.
    - inventory_select_existing_accessory.ejs: Vista para mostrar al administrador los accesorios existentes (Front)

--- src/controllers/admin_controller.js
+++ src/controllers/admin_controller.js
@@ -9,4 +9,16 @@ controller.inventory_add_new = (req, res) => {
     res.render('admin/new_accessory');   
 }; 
 
+controller.inventory_add_existing = (req, res) => {
+    res.render('admin/inventory_select_existing_accessory');   
+}; 
+
+controller.inventory_add_existing_id = (req, res) => {
+
+    const { id } = req.body; 
+    console.log(id);
+
+    res.render('admin/existing_accessory');   
+}; 
+
 module.exports = controller; 
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,5 +4,7 @@ const controller = require('../controllers/admin_controller');
 
 router.get('/inventory', controller.inventory); 
 router.get('/inventory/add_new', controller.inventory_add_new); 
+router.get('/inventory/add_existing', controller.inventory_add_existing); 
+router.get('/inventory/add_existing/:id', controller.inventory_add_existing_id); 
 
 module.exports = router; 
