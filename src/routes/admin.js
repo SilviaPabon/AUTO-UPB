3827ae1d819e54ebf82ccb235e50959e4c7627c6
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2022-02-25 01:07:13 -0500

    backend of view and procedure in database
    cambios en:
    -query_procedures.sql (se crea procedimiento de inserción desde l vista de creación por parte de admin)
    -passport.js (se crea apartado backend con encriptación (contraseña), de los datos que se registren desde esta vista)
    -admin.js (se anexa passport y se agrega la ruta post)

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,9 +1,13 @@
 const express = require('express'); 
 const router = express.Router(); 
 const controller = require('../controllers/admin_controller');  
-
+const passport = require('passport');
 
 router.get('/create_account', controller.createAccount); 
-
+router.post('/create_account',  passport.authenticate('local.adminSignup', {
+    successRedirect: '/',
+    failureRedirect: '/create_account',
+    failureFlash: true,
+})); 
 
 module.exports = router;
