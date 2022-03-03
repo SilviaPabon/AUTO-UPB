f40d3895ab51a899c0fc8db4c365152a0faa75c1
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-03-03 09:01:50 -0500

    Listado de productos destacados
    Cambios en:
    
    - general_routes.js: Se traen los productos destacados desde la base de datos.
    - _home.scss: Estilos de las cards para accesorios destacados.
    - index.ejs: Se renderizan los accesorios traídos desde el backend.

--- src/controllers/general_routes.js
+++ src/controllers/general_routes.js
@@ -1,7 +1,23 @@
 const controller = {}; 
+const pool = require('../database/connection');
 
-controller.home = (req, res) => {
-    res.render('index'); 
+controller.home = async(req, res) => {
+
+    // Se obtienen los productos en descuento
+    const disccountProducts = await pool.query('CALL SHOW_TOP_DISCOUNT()'); 
+
+    // Se obtienen los productos top más vendidos
+    const featuredProducts = await pool.query('CALL SHOW_TOP_SALES()'); 
+
+    console.log(featuredProducts); 
+    console.log(featuredProducts[0][0]['nombre']); 
+
+    const data = {
+        disccountProducts, 
+        featuredProducts
+    }
+    
+    res.render('index', {data}); 
 }; 
 
 controller.accessories = (req, res) => {
