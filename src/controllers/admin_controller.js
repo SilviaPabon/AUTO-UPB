b75de2474ce0fdb8dda2670dabfb22bfa68b52af
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2022-03-03 23:03:59 -0500

    dev of post method in /inventory/add_new
    cambios en:
    -admin_controller.js
    -routes/admin.js
    Estas modificaciones permiten insertar nuevos accesorios en la base de datos

--- src/controllers/admin_controller.js
+++ src/controllers/admin_controller.js
@@ -1,5 +1,5 @@
 const controller = {}; 
-// const pool = require('../database/connection');
+const pool = require('../database/connection');
 
 controller.createAccount = (req, res) => {
     res.render('auth/create_acc_admin');
@@ -11,7 +11,44 @@ controller.inventory = (req, res) => {
 
 controller.inventory_add_new = (req, res) => {
     res.render('admin/new_accessory');   
-}; 
+};
+
+controller.inventory_add_new_post = async (req, res) => {
+
+    const {
+        name,
+        description,
+        buyprice,
+        sellprice,
+        discount,
+        inventory,
+    } = req.body;
+    console.log(req.user.id_usuario);
+    const newAcc = {
+        usID: req.user.id_usuario,
+        name,
+        description,
+        inventory,
+        buyprice,
+        sellprice,
+        discount,
+    };
+
+    //db
+    await pool.query('CALL ADD_NEW_ACCESSORY(?, ?, ?, ?, ?, ?, ?)', [
+        newAcc.usID,
+        newAcc.name,
+        newAcc.description,
+        newAcc.inventory,
+        newAcc.buyprice,
+        newAcc.sellprice,
+        newAcc.discount,
+    ]);
+
+    //
+    req.flash('success', 'Transacción: Accesorio Añadido'); 
+    res.redirect('/admin/inventory/add_existing');
+};
 
 controller.inventory_add_existing = (req, res) => {
     res.render('admin/inventory_select_existing_accessory');   
@@ -22,7 +59,7 @@ controller.inventory_add_existing_id = (req, res) => {
     const { id } = req.body; 
     console.log(id);
 
-    res.render('admin/existing_accessory');   
+    res.render('admin/inventory');   
 }; 
 
 module.exports = controller; 
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -12,6 +12,7 @@ router.post('/create_account',  passport.authenticate('local.adminSignup', {
 
 router.get('/inventory', controller.inventory); 
 router.get('/inventory/add_new', controller.inventory_add_new); 
+router.post('/inventory/add_new', controller.inventory_add_new_post); 
 router.get('/inventory/add_existing', controller.inventory_add_existing); 
 router.get('/inventory/add_existing/:id', controller.inventory_add_existing_id);  
 
