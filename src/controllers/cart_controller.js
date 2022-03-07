8db631df47046ab8fc05eb66e8f67bf9b24bb628
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-03-07 18:47:34 -0500

    Actualización del controlador del carrito
    Se agregó el uso de las alertas pop-up en el carrito y validaciones extras de stock para la acción de agregar elemento al carrito.

--- src/controllers/cart_controller.js
+++ src/controllers/cart_controller.js
@@ -4,61 +4,67 @@ const connection = require('../database/connection');
 // -----------
 // Ruta para añadir un accesorio al carrito
 controller.cartAdd = async (req, res) => {
-
     const { id } = req.body;
     console.log(id);
+
+    // Inicializa el objeto y las variables de control
+    let item = {};
+    let success = false;
+    let itemIndexOnCart = -1;
 
     // Se obtiene la variable global del carrito
     const cart = req.app.locals.cart;
 
-    // Consulta a la base de datos para obtener el accesorio
-    const queryResponse = await connection.query('CALL SHOW_ACCESSORY_DETAILS(?)', [id]);
-
-    // Inicializa el objeto y las variables de control
-    let item = {};
-    let success = false; 
-    let itemIndexOnCart = -1; 
-
     // Revisa si ya existe el accesorio en el carrito
-    for(let index = 0; index < cart.length; index++){
-        if(cart[index].id_accesorio == id){
-            itemIndexOnCart = index; 
-            break; 
+    for (let index = 0; index < cart.length; index++) {
+        if (cart[index].id_accesorio == id) {
+            itemIndexOnCart = index;
+            break;
         }
     }
 
-    if(itemIndexOnCart == -1){
+    if (itemIndexOnCart == -1) {
         //Si el accesorio no existe en el carrito, lo agrega
-        try { 
-            item.id_accesorio = queryResponse[0][0].id_accesorio; 
-            item.nombre = queryResponse[0][0].nombre; 
-            item.stock = queryResponse[0][0].stock;
-            item.precio_base = queryResponse[0][0].precio_base;
-            item.descuento = queryResponse[0][0].descuento;
-            item.precio_final = queryResponse[0][0].precio_final;
-            item.ruta_imagen = queryResponse[0][0].ruta_imagen;
-            item.cantidad = 1;
+        try {
+            // Consulta a la base de datos para obtener el accesorio
+            const queryResponse = await connection.query('CALL SHOW_ACCESSORY_DETAILS(?)', [id]);
 
-            cart.push(item); 
+            if(queryResponse[0][0].stock >= 1){
+                item.id_accesorio = queryResponse[0][0].id_accesorio;
+                item.nombre = queryResponse[0][0].nombre;
+                item.stock = queryResponse[0][0].stock;
+                item.precio_base = queryResponse[0][0].precio_base;
+                item.descuento = queryResponse[0][0].descuento;
+                item.precio_final = queryResponse[0][0].precio_final;
+                item.ruta_imagen = queryResponse[0][0].ruta_imagen;
+                item.cantidad = 1;
 
-            success= true; 
+                cart.push(item);
+
+                success = true;
+            }
+
         } catch (error) {
-            success = false; 
+            success = false;
         }
-    }else{
+    } else {
         //Si el accesorio ya existe en el carrito, le agrega una unidad
-        item = cart[itemIndexOnCart]; 
-        item.cantidad += 1; 
-        success = true; 
+        item = cart[itemIndexOnCart];
+        
+        //Revisa que aún tenga stock
+        if(item.stock > item.cantidad){
+            item.cantidad += 1;
+            success = true;
+        }
     }
 
     // Se envía la respuesta según si la operación fue exitosa o no
-    success == true ? 
-        res.status(200).json({
-            status : "El accesorio fue agregado exitosamente"
-        }) :
-        res.status(404).json({
-            status : "No se econtró el accesorio dado"
+    success == true
+        ? res.status(200).json({
+            status: 'El accesorio fue agregado exitosamente',
+        })
+        : res.status(404).json({
+            status: 'No se econtró el accesorio dado',
         });
 };
 
