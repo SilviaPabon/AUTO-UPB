cc004eaf897b541cc8e0ac2db0da305aaeab9623
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2022-03-09 16:21:31 -0500

    Backend for update state of existing accounts
    cambios en:
    admin_controller.js métodos para mostrar los datos de la cuenta seleccionada y para actualizar al nuevo estado.
    
    routes/admin.js rutas get y post
    
    admin/existing_accounts.js para dirigir a la interfaz donde se puede cambiar el estado

--- src/controllers/admin_controller.js
+++ src/controllers/admin_controller.js
@@ -35,4 +35,40 @@ controller.accounts = async (req, res) => {
     res.render('admin/existing_accounts', {data}); 
 }
 
+controller.state_acc = async (req, res) => {
+
+    const { id } = req.params;
+    const userd = {usID: req.user.id_usuario};
+
+    const user = await pool.query('CALL SHOW_ACCOUNT_DETAILS(?, ?)', [id, userd.usID]);
+    const data = {
+        user
+    }
+    res.render('admin/existing_accounts_state', {data});   
+}; 
+
+controller.state_acc_post = async (req, res) => {
+
+    const { id } = req.params;
+
+    const { state } = req.body;
+
+    //Create new plan object
+    const modifyState = {
+        usID: req.user.id_usuario,
+        id,
+        state,
+    };
+
+    await pool.query('CALL CHANGE_EXISTING_USER_STATUS(?, ? , ?)', [
+        modifyState.usID,
+        modifyState.id,
+        modifyState.state
+    ]);
+
+    req.flash('success', 'Transacción exitosa: ');
+    res.redirect('/admin/accounts')
+
+}
+
 module.exports = controller; 
