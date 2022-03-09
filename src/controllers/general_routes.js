e3037e96396bbd0582516ea8e1cff62a5e015b25
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-03-09 12:22:32 -0500

    Nueva vista y nuesvas rutas
    Se creó la vista para ver los detalles de un accesorio a partir de su id.

--- src/controllers/general_routes.js
+++ src/controllers/general_routes.js
@@ -1,26 +1,39 @@
-const controller = {}; 
+const controller = {};
 const pool = require('../database/connection');
 
-controller.home = async(req, res) => {
-
+controller.home = async (req, res) => {
     // Se obtienen los productos en descuento
-    const disccountProducts = await pool.query('CALL SHOW_TOP_DISCOUNT()'); 
+    const disccountProducts = await pool.query('CALL SHOW_TOP_DISCOUNT()');
 
     // Se obtienen los productos top más vendidos
-    const featuredProducts = await pool.query('CALL SHOW_TOP_SALES()'); 
+    const featuredProducts = await pool.query('CALL SHOW_TOP_SALES()');
 
     const data = {
-        disccountProducts, 
-        featuredProducts
+        disccountProducts,
+        featuredProducts,
+    };
+
+    res.render('index', { data });
+};
+
+controller.accessories = async (req, res) => {
+    const products = await pool.query('CALL SHOW_ACCESSORIES');
+    res.render('products', { products });
+};
+
+controller.accessoryDetails = async (req, res) => {
+    const { id } = req.params;
+
+    const product = await pool.query('CALL SHOW_ACCESSORY_DETAILS(?)', [id]);
+
+    if(product[0].length != 0){
+        console.table(product[0]); 
+        res.render('productDetails', { product }); 
+    }else{
+        req.flash('message', 'ERROR: No se encontró el accesorio solicitado'); 
+        res.redirect('/'); 
     }
-    
-    res.render('index', {data}); 
-}; 
 
-controller.accessories = async(req, res) => {
+};
 
-    const products = await pool.query('CALL SHOW_ACCESSORIES')
-    res.render('products', {products}); 
-}
-
-module.exports = controller; 
+module.exports = controller;
--- src/routes/general_routes.js
+++ src/routes/general_routes.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/general_routes.js');
 
 router.get('/', controller.home); 
 router.get('/accessories', controller.accessories)
+router.get('/accessories/:id', controller.accessoryDetails); 
 
 module.exports = router;
