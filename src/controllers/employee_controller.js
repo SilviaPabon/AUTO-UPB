92df538da03dd2f0f7f8e3167f13d296fde1030e
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-03-19 09:13:47 -0500

    FUNCIONALIDAD PARA GENERAR ORDEN DE COMPRA
    Se agregó el controlador para generar la orden de compra.

--- src/controllers/employee_controller.js
+++ src/controllers/employee_controller.js
@@ -1,5 +1,5 @@
 const controller = {};
-const { response } = require('express');
+const helpers = require('../libs/helpers');
 const connection = require('../database/connection');
 
 // ------
@@ -58,11 +58,87 @@ controller.user_exists = async (req, res) => {
 // ----
 // Ruta para enviar los datos del cliente y proceder con la orden
 controller.postOrder = async (req, res) => {
-    const { name, documento, email, phone, address, password, conditions } = req.body;
 
-    // Si ya existe el usuario en la base de datos toma su id (Revisa si ya existe el correo o el documento)
+    //Variables de control
+    let userStepSuccess = false; 
+    let orderStepSuccess = false; 
 
-    res.send('Received');
+    //Recibimiento y organización de los datos
+    const { id_usuario, name, documento, email, phone, address, password, conditions } = req.body;
+
+    const user = {
+        id_usuario,
+        name, 
+        documento, 
+        email, 
+        phone, 
+        address, 
+        password, 
+        conditions
+    }
+
+    const order = {
+        id_orden: -1
+    }
+
+    user.conditions = user.conditions == 'on' ? 1 : 0;
+
+    if(user.id_usuario != -1){
+        //Si el usuario existe, verifica que la contraseña sea correcta
+        const userPassword = await connection.query('CALL GET_USER_PASSWORD(?)', [user.email]); 
+        const passwordsAreEqual = await helpers.matchPassword(user.password, userPassword[0][0].contraseña);
+
+        if(passwordsAreEqual){
+            userStepSuccess = true; 
+        }else{
+            req.flash('message', 'Error: La contraseña del usuario no coincide');
+            res.redirect('/employee/cart'); 
+        }
+    }else{
+        //Si el usuario no existe, lo crea en la base de datos y obtiene su id
+
+        //Se encripta la contraseña para su almacenamiento en la base de datos
+        user.password = await helpers.encryptPassword(user.password); 
+
+        //Se realiza el registro
+        const newUserQuery = await connection.query('CALL REGISTER_NEW_CLIENT(?, ?, ?, ?, ?, ?, ?)', [
+            user.name, 
+            user.documento, 
+            user.email, 
+            user.address, 
+            user.phone, 
+            user.conditions, 
+            user.password
+        ]); 
+
+        //Reemplaza el id que estaba como -1 al nuevo id del usuario creado
+        user.id_usuario = newUserQuery[0][0].id_usuario; 
+
+        userStepSuccess = true; 
+    }
+
+
+    //Si el paso del cliente salió bien, se crea la orden de compra a nombre de ese cliente
+    if(userStepSuccess){
+        const newBuyOrderQuery = await connection.query('CALL REGISTER_NEW_BUY_ORDER(?, ?)', [req.user.id_usuario, user.id_usuario]); 
+        order.id_orden = newBuyOrderQuery[0][0].id_orden; 
+        orderStepSuccess = true; 
+    }
+
+
+    //Una vez se cree la orden de compra, se comienza la transacción de registro de la orden
+    if(orderStepSuccess){
+        const transactionQuery = await connection.query('CALL register_buy_order_from_cart(?, ?)', [req.user.id_usuario, order.id_orden]); 
+        console.log(transactionQuery); 
+
+        if(transactionQuery[0] == undefined){
+            req.flash('success', 'Proceso exitoso: Se generó la orden de compra de manera exitosa');
+            res.redirect('/employee/cart'); 
+        }else{
+            req.flash('message', `Error: El stock del accesorio ${transactionQuery[0][0].nombre} es ${transactionQuery[0][0].stock}`);
+            res.redirect('/employee/cart'); 
+        }
+    }
 };
 
 module.exports = controller;
