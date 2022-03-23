0198925c3190cb47985593ac75b457ac6d8c409e
diegoaach <61645376+diegoaach@users.noreply.github.com>
2022-03-22 23:43:47 -0500

    us18
    Modificaciones
    partner_controller: Se añadieron los controladores asociados al socio
    partner: se configuraron las routes
    navbar: Configurado el acceso a la vista desde el boton de Cuentas de clientes que aparece con la cuenta de socio
    existing_accounts_partner: Creacion de la vista

--- src/controllers/partner_controller.js
+++ src/controllers/partner_controller.js
@@ -1,6 +1,32 @@
 const controller = {}; 
-// const pool = require('../database/connection');
+const pool = require('../database/connection');
 
+controller.accountsPartner = async(req, res) => {
+    const users = await pool.query('CALL SHOW_ACCOUNTS_PARTNERS (?)', [req.user.id_usuario]);
+    const data = {users, isFiltered : false};
+    
+    res.render('partner/existing_accounts_partner', {data} );
+}
 
+controller.searchAccountsPartner = async (req, res) => {
+    // Se toma el criterio de busqueda
+    const { criteria } = req.body;
+    res.redirect(`/partner/accounts/${criteria}`);
+}
+
+controller.searchAccountsResultPartner = async (req, res) => {
+    
+    const { criteria } = req.params;
+
+    const users = await pool.query('CALL PARTNER_SEARCH_USER_FROM_CRITERIA(?, ?)', [req.user.id_usuario ,criteria]);
+
+    const data = {
+        users,
+        isFiltered : true,
+        criteria
+    };
+
+    res.render('partner/existing_accounts_partner', { data });
+}
 
 module.exports = controller; 
