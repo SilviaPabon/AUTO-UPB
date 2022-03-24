9fee154120504bdb71ba2b7e7e8b2d91977543d2
danielcadena02 <62720871+danielcadena02@users.noreply.github.com>
2022-03-24 10:36:07 -0500

    visualizacion de inventario existente para los socios

--- src/controllers/partner_controller.js
+++ src/controllers/partner_controller.js
@@ -1,6 +1,38 @@
-const controller = {}; 
-// const pool = require('../database/connection');
+const controller = {};
+const pool = require('../database/connection');
 
+controller.inventory=async(req,res)=>{
+    const inventory = await pool.query('CALL SHOW_ACCESSORIES_INTERNAL(?)', [req.user.id_usuario]);
+    const data = {
+        ACCESORIOS: inventory[0],
+        isFiltered:false
+    }
+    res.render('partner/partners_inventory', {
+        data
+    });
+}
 
+// Ruta para cuando se busca un usuario
+controller.searchinventory = async (req, res) => {
+    //La ruta solo toma lo que se pasó en el formulario y lo redirige a la vista
+    const { criteria } = req.body;
+    res.redirect(`/partner/inventory/${criteria}`);
+};
+
+// Ruta para mostrar los usuarios resultantes de la búsqueda
+controller.searchinventoryResult = async (req, res) => {
+    const { criteria } = req.params;
+
+    const inventory = await pool.query('CALL SEARCH_ACCESSORIES_FROM_CRITERIA_INTERNAL(?, ?)', [req.user.id_usuario, criteria]);
+
+    const data = {
+        ACCESORIOS: inventory[0],
+        isFiltered : true,
+        criteria
+    };
+
+    res.render('partner/partners_inventory', { data });
+};
 
 module.exports = controller; 
+
--- src/routes/partner.js
+++ src/routes/partner.js
@@ -4,5 +4,9 @@ const controller = require('../controllers/partner_controller');
 const protect = require('../libs/protect_middlewares'); 
 
 
+router.get('/inventory', protect.isLoggedIn, protect.isPartner, controller.inventory);
+router.post('/inventory', protect.isLoggedIn, protect.isPartner,controller.searchinventory); 
+router.get('/inventory/:criteria',  protect.isLoggedIn, protect.isPartner,controller.searchinventoryResult);
+
 
 module.exports = router;
