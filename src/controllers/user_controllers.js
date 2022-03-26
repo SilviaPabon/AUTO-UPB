724cc7ce97bf3911f64c04807d66499ed53f5c54
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-03-26 17:43:15 -0500

    Show client their orders
    Cambios en:
    
    - user_controller.js: Consulta a la base de datos para mostrar al cliente sus órdenes de compra.
    
    - orders_clients.ejs: Renderizar las órdenes de compra del cliente.

--- src/controllers/user_controllers.js
+++ src/controllers/user_controllers.js
@@ -1,8 +1,25 @@
 const controller = {};
-const connection = require('../database/connection');
+const pool = require('../database/connection');
 
 controller.orderClient = async (req, res) => {
-    res.render('clients/orders_clients');
+    let queryOk = false;
+
+    try {
+        var clientOrders = await pool.query('CALL GET_USER_ORDERBUY_FROM_ID(?)', [req.user.id_usuario]);
+        queryOk = true;
+    } catch (error) {
+        queryOk = false;
+    }
+
+    if (queryOk) {
+        const data = clientOrders[0];
+        console.log(req.user.id_usuario); 
+
+        res.render('clients/orders_clients', {data});
+    } else {
+        req.flash('error', 'Error inesperado en la consulta a la base de datos');
+        res.redirect('/');
+    }
 };
 
 module.exports = controller;
