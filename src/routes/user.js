9876306aa0fa66ada0fc5de66b977298a48e240d
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2022-04-15 10:49:59 -0500

    update in routes files (format)

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,10 @@
-const express = require('express'); 
-const router = express.Router(); 
-const protect = require('../libs/protect_middlewares')
-const controller = require('../controllers/user_controllers'); 
+const express = require('express');
+const router = express.Router();
+const protect = require('../libs/protect_middlewares');
+const controller = require('../controllers/user_controllers');
 
 router.get('/orders', protect.isLoggedIn, controller.orderClient);
 router.get('/orderDone/:id', protect.isLoggedIn, controller.orderDone);
-router.get('/bill/:id', protect.isLoggedIn, controller.user_bill); 
+router.get('/bill/:id', protect.isLoggedIn, controller.user_bill);
 
 module.exports = router;
-
