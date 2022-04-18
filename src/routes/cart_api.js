8365177cb3e8be5cfad16155dfbc7f9af4169a56
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-04-18 08:32:28 -0500

    Fix redirection on /employee/cart

--- src/routes/cart_api.js
+++ src/routes/cart_api.js
@@ -4,16 +4,16 @@ const protect = require('../libs/protect_middlewares');
 const controller = require('../controllers/cart_controller');
 
 // Ruta para agregar un accesorio al carrito
-router.post('/add', protect.isLoggedIn, protect.canUseCart,controller.cartAdd);
+router.post('/add', protect.isLoggedIn, controller.cartAdd);
 
 // Ruta para eliminar elemento del carrito
-router.get('/remove/:id', protect.isLoggedIn, protect.canUseCart,controller.cartRemoveGet);
+router.get('/remove/:id', protect.isLoggedIn, controller.cartRemoveGet);
 
 // Ruta para mostrar el carrtito de compras
 router.get('/', protect.isLoggedIn, protect.canUseCart,controller.showCart);
 
 // Ruta para actualizar el número items de un accesorio en el carrito
-router.post('/update', protect.isLoggedIn, protect.canUseCart,controller.cartUpdate);
+router.post('/update', protect.isLoggedIn, controller.cartUpdate);
 
 // Ruta para emitir la órden de compra
 router.get('/buy', protect.isLoggedIn, protect.canUseCart,controller.orderClientPost);
